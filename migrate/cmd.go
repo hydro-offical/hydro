3408a16875cbeea5c837bad5490e09ec34a82ae4
hydro-offical <82814248+hydro-offical@users.noreply.github.com>
2021-04-19 19:37:26 +0800

    Add files via upload

---
+++ migrate/cmd.go
@@ -0,0 +1,51 @@
+package migrate
+
+import (
+	"fmt"
+
+	"github.com/kava-labs/kava/migrate/v0_14"
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/server"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/version"
+
+	tmtypes "github.com/tendermint/tendermint/types"
+)
+
+// MigrateGenesisCmd returns a command to execute genesis state migration.
+func MigrateGenesisCmd(_ *server.Context, cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "migrate [genesis-file]",
+		Short:   "Migrate genesis file from kava v0.11 (or v0.12) to v0.14",
+		Long:    "Migrate the source genesis into the current version, sorts it, and print to STDOUT.",
+		Example: fmt.Sprintf(`%s migrate /path/to/genesis.json`, version.ServerName),
+		Args:    cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			importGenesis := args[0]
+			genDoc, err := tmtypes.GenesisDocFromFile(importGenesis)
+			if err != nil {
+				return fmt.Errorf("failed to read genesis document from file %s: %w", importGenesis, err)
+			}
+
+			newGenDoc := v0_14.Migrate(*genDoc)
+
+			bz, err := cdc.MarshalJSONIndent(newGenDoc, "", "  ")
+			if err != nil {
+				return fmt.Errorf("failed to marshal genesis doc: %w", err)
+			}
+
+			sortedBz, err := sdk.SortJSON(bz)
+			if err != nil {
+				return fmt.Errorf("failed to sort JSON genesis doc: %w", err)
+			}
+
+			fmt.Println(string(sortedBz))
+			return nil
+		},
+	}
+
+	return cmd
+}
